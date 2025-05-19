8be7c047ad5678919db8362ebc3c7bad4ad34dfb
LeoBP1987 <leonardobp1987@gmail.com>
2025-05-19 19:13:27 -0300

    Fix: Ajustando Layout Botão Agenda Mobile 3

--- src/componentes/BotaoData/index.tsx
+++ src/componentes/BotaoData/index.tsx
@@ -18,7 +18,7 @@ const InputAgendaEstilizado = styled.input`
     cursor: pointer;
     @media screen and (max-width: 800px) {
         background: #FFFFFF url('/imagens/icones/calendario.png') no-repeat right 6px center;
-        background-size: 22px 22px;
+        background-size: 25px 25px;
     }
 `
 
